23eceb00ccc4d65f09ac9a71d3e78a61834771fb
Carlitos Francisco <francisco.carlitos12@gmail.com>
2023-08-16 20:54:29 +0400

    +initialized apps to be used

---
+++ apps/event-bus-service-c/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'event-bus-service-c',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/event-bus-service-c',
+};
